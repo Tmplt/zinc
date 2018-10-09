47fc662748e5922a9b0ea5564eccea0380bbe694
Tmplt <tmplt@dragons.rocks>
2018-10-09 15:40:39 +0200

    Printer: properly generate branch instructions

--- Printer.ml
+++ Printer.ml
@@ -6,9 +6,9 @@ open State__State
 open Vm__Vm
 open Env
 open Common
+open Z
 
 (* TODO:
- *  - Fix branch instructions (they cannot take relative addresses)
  *  - Add comment above every chunk explaining what it does
  *  - Format generated assembly to be more readable
  *      - Indent with 8 spaces, making space for eventual labels
@@ -27,7 +27,7 @@ let pushi (n:Z.t) =
 (* pop value into passed register (temporary) *)
 let pop (reg:int) =
   "lw $t" ^ string_of_int reg ^ ", 0($sp)" ^ nl ^
-  decstack ^ nl
+  decstack
 
 (* pop value into passed register (without changing $sp) *)
 let pop' (reg:int) =
@@ -39,8 +39,10 @@ let ofs_of_id id =
   | _ -> assert false
 
 let string_of_reg idr = "$" ^ Z.to_string idr
+let string_of_label label = "label_" ^ Z.to_string label ^ ":" ^ nl
+let string_of_label' label = "label_" ^ Z.to_string label ^ nl
 
-let of_instr = function
+let of_instr currlabel = function
   (* new instructions, register based *)
   | Iload (idr, id) -> (* load register with variable *)
     "lw $" ^ Z.to_string idr ^ ", " ^ ofs_of_id id ^ "($gp)"
@@ -49,10 +51,10 @@ let of_instr = function
   | Istore (idr, id) -> (* store a register to variable *)
     "sw $" ^ Z.to_string idr ^ ", " ^ ofs_of_id id ^ "($gp)"
   | Ipushr idr       -> (* push register on stack *)
-    incstack ^ nl ^
+    incstack ^
     "lw $" ^ Z.to_string idr ^ ", 0($sp)"
   | Ipopr idr        -> (* pop from stack into register *)
-    "lw $" ^ Z.to_string idr ^ ", 0($sp)" ^ nl ^
+    "lw $" ^ Z.to_string idr ^ ", 0($sp)" ^
     decstack
   | Iaddr (lhs, rhs, res) -> (* add two registers, store result in third *)
     "add " ^ string_of_reg res ^ ", " ^ string_of_reg lhs ^ ", " ^ string_of_reg rhs
@@ -61,17 +63,17 @@ let of_instr = function
   | Isubr (lhs, rhs, res) -> (* subtract two registers, store result in third *)
     "sub " ^ string_of_reg res ^ ", " ^ string_of_reg lhs ^ ", " ^ string_of_reg rhs
   | Ibeqr (lhs, rhs, ofs) -> (* skip ofs forward if lhs = rhs *)
-    "beq " ^ string_of_reg lhs ^ ", " ^ string_of_reg rhs ^ ", " ^ Z.to_string ofs
+    "beq " ^ string_of_reg lhs ^ ", " ^ string_of_reg rhs ^ ", " ^ string_of_label' (currlabel + ofs)
   | Ibner (lhs, rhs, ofs) -> (* skip ofs forward if lhs != rhs *)
-    "bne " ^ string_of_reg lhs ^ ", " ^ string_of_reg rhs ^ ", " ^ Z.to_string ofs
+    "bne " ^ string_of_reg lhs ^ ", " ^ string_of_reg rhs ^ ", " ^ string_of_label' (currlabel + ofs)
   | Ibler (lhs, rhs, ofs) -> (* skip ofs forward if lhs != rhs *)
     (* lhs <= rhs -> rhs > lhs -> rhs - lhs > 0 *)
     "sub $t0, " ^ string_of_reg rhs ^ ", " ^ string_of_reg lhs ^ nl ^
-    "bgtz $t0, " ^ Z.to_string ofs
+    "bgtz $t0, " ^ string_of_label' (currlabel + ofs)
   | Ibgtr (lhs, rhs, ofs) -> (* skip ofs forward if lhs > rhs *)
     (* lhs > rhs -> lhs - rhs > 0 *)
     "sub $t0, " ^ string_of_reg lhs ^ ", " ^ string_of_reg rhs ^ nl ^
-    "bgtz $t0, " ^ Z.to_string ofs
+    "bgtz $t0, " ^ string_of_label' (currlabel + ofs)
 
   (* original/old instructions for stack machine *)
   | Iconst n        -> (* push n on stack *)
@@ -82,7 +84,7 @@ let of_instr = function
     pop 1 ^
     "sw $t1, " ^ ofs_of_id id ^ "($gp)"
   | Ibranch ofs     -> (* skip ofs instructions *)
-    "b " ^ Z.to_string ofs
+    "b " ^ string_of_label' (currlabel + ofs)
   | Iadd            -> (* pop two values, push their sum *)
     (* pop the values *)
     pop 1 ^
@@ -106,35 +108,38 @@ let of_instr = function
   | Ibeq ofs        -> (* pop a, b, skip ofs forward if a = b *)
     pop 1 ^
     pop 2 ^
-    "beq $t1, $t2, " ^ Z.to_string ofs
+    "beq $t1, $t2, " ^ string_of_label' (currlabel + ofs)
   | Ibne ofs        -> (* pop a, b, skip ofs forward if a != b *)
     pop 1 ^
     pop 2 ^
-    "bne $t1, $t2, " ^ Z.to_string ofs
+    "bne $t1, $t2, " ^ string_of_label' (currlabel + ofs)
   | Ible ofs        -> (* pop a, b, skip ofs forward if b <= a *)
     pop 1 ^ (* a *)
     pop 2 ^ (* b *)
     (* b <= a -> a > b -> a - b > 0 *)
     "sub $t0, $t1, $t2" ^ nl ^
-    "bgtz $t0, " ^ Z.to_string ofs
+    "bgtz $t0, " ^ string_of_label' (currlabel + ofs)
   | Ibgt ofs        -> (* pop a, b, skip ofs forward if b > a *)
     pop 1 ^ (* a *)
     pop 2 ^ (* b *)
     (* b > a -> b - a > 0 *)
     "sub $t0, $t2, $t1" ^ nl ^
-    "bgtz $t0, " ^ Z.to_string ofs
+    "bgtz $t0, " ^ string_of_label' (currlabel + ofs)
   | Ihalt           -> (* end of program, loop forevermore *)
-    "b 0"
+    "b " ^ string_of_label' currlabel
 
 (* TODO: final newline is applied here, strip it from of_instr above *)
 let of_code code =
-  let rec of_code' = function
+  let rec of_code' c labelid =
+    match c with
     | [] -> ""
-    | i :: il -> of_instr i ^ nl ^ nl ^ of_code' il
+    | i :: il ->
+        string_of_label labelid ^
+        of_instr labelid i ^ nl ^ nl ^ of_code' il (labelid + (Z.of_int 1))
   in
   ".text" ^ nl ^
   ".set noreorder" ^ nl ^ nl ^
-  of_code' code
+  of_code' code (Z.of_int 0)
 
 (* vim: shiftwidth=2:
 *)
